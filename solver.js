b6cc7e8eb0fcac9144d007852ac7cb6ba1c7837a
kenave <kaverbookh@gmail.com>
2020-05-15 13:48:43 -0400

    implemented checkInSquare function and "possible" helper function

--- solver.js
+++ solver.js
@@ -3,7 +3,7 @@ function solver(sudoku){
   takes in a 2d array representation of a sudoku
     format is [[row], [row], [row], [row], [row], [row], [row], [row], [row]]
     empty spaces are represented by 0
-  recursive function?
+  recursive function? maybe we can refactor after we get it working
   returns the solved sudoku
 
   basic sudoku rules:
@@ -13,11 +13,11 @@ function solver(sudoku){
   need 2 helper functions:
     one that checks for a number in a column
     one that checks for a number in a square
-    might as well make one to check for a number in a column?
+    might as well make one to check for a number in a row
   */
 
   function checkInColumn(column, target){
-    /* checks for a target number in the given column of the sudoku array
+    /* checks for a target number in the given column of the sudoku array, returns bool
     how to traverse the columns?
     first column:
     [0][0]
@@ -32,9 +32,17 @@ function solver(sudoku){
     return false
   }
 
+  function checkInRow(row, target){
+    // checks for a target number in the specified row of the sudoku array, returns bool
+    for (let i = 0; i < 9; i++){
+      if (sudoku[row - 1][i] === target) return true
+    }
+    return false
+  }
+
   function checkInSquare(square, target){
-    /* checks for a target number in the given column of the sudoku array
-    need to access the 3x3 squares (hereforth referred to as a "square")
+    /* checks for a target number in the given column of the sudoku array, returns bool
+    need to access the 3x3 squares (henceforth referred to as a "square")
     first square:
      x  y
     [0][0]...[0][2]
@@ -56,35 +64,57 @@ function solver(sudoku){
     for square 8: x from 3 - 5, y from 6 - 8
     for square 9: x from 6 - 8, y from 6 - 8
     */
+    const squareArray = [
+      [0,0], [3,0], [6,0],
+      [0,3], [3,3], [6,3],
+      [0,6], [3,6], [6,6]
+    ]
+    const lowerBoundX = squareArray[square - 1][0]
+    const upperBoundX = lowerBoundX + 3
+    const lowerBoundY = squareArray[square - 1][1]
+    const upperBoundY = lowerBoundY + 3
 
-    for (let x = 0; x < 3; x++) { // only checks square 1 right now
-      for (let y = 0; y < 3; y++){
-        if (sudoku[x][y] === target) return true
+    for (let row = lowerBoundX; row < upperBoundX; row++) {
+      for (let column = lowerBoundY; column < upperBoundY; column++){
+        // console.log(sudoku[column][row])
+        if (sudoku[column][row] === target) return true
       }
     }
     return false
   }
+  
+  const completed = 45  // summation of 1 to 9 may be a useful check for completed rows, columns, and squares
 
-  console.log(checkInColumn(1, 7)) // true
-  console.log(checkInColumn(5, 8)) // true
-  console.log(checkInColumn(9, 4)) // false
+  // now to do the solving part
+  // can start with checking for a number in the first row and column and square
+  function possible(x, y, num) {
+    if (sudoku[y][x] !== 0) return false
+    if (checkInColumn(x, num)) return false
+    if (checkInRow(y, num)) return false
+    const x0 = Math.ceil((x-1) / 3)
+    const y0 = Math.ceil((y-1) / 3)
+    const square = x0 + (3 * (y0-1))
+    if (checkInSquare(square, num)) return false
+    return true
+  }
 
-  console.log(checkInSquare(1, 2)) // true
-  console.log(checkInSquare(1, 3)) // false
+  console.log(possible(2, 3, 6)) // false
+  console.log(possible(5, 8, 9)) // false
+  console.log(possible(5, 8, 7)) // true
 
-  console.log('hello')
 }
 
 let sudoku_easy = [
-  [0, 9, 1, 2, 5, 7, 6, 0, 0],
-  [0, 2, 6, 0, 1, 4, 0, 9, 8],
-  [0, 0, 7, 0, 0, 0, 0, 0, 1],
-  [0, 1, 0, 5, 6, 9, 4, 8, 0],
-  [7, 0, 5, 1, 4, 0, 9, 3, 0],
-  [4, 0, 0, 0, 0, 0, 0, 6, 0],
-  [6, 0, 0, 0, 8, 0, 0, 0, 9],
-  [0, 5, 0, 0, 0, 3, 2, 0, 0],
-  [0, 3, 0, 9, 0, 0, 8, 0, 0]
+//x  1  2  3  4  5  6  7  8  9      y
+    [0, 9, 1, 2, 5, 7, 6, 0, 0], // 1
+    [0, 2, 6, 0, 1, 4, 0, 9, 8], // 2
+    [0, 0, 7, 0, 0, 0, 0, 0, 1], // 3
+    [0, 1, 0, 5, 6, 9, 4, 8, 0], // 4
+    [7, 0, 5, 1, 4, 0, 9, 3, 0], // 5
+    [4, 0, 0, 0, 0, 0, 0, 6, 0], // 6
+    [6, 0, 0, 0, 8, 0, 0, 0, 9], // 7
+    [0, 5, 0, 0, 0, 3, 2, 0, 0], // 8
+    [0, 3, 0, 9, 0, 0, 8, 0, 0]  // 9
 ]
 
 let sudoku_medium = [
