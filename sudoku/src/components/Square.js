34b9a0828e41f66943d2ed0fe3fd3d97e6cbcd16
kenave <kaverbookh@gmail.com>
2020-05-28 13:39:05 -0400

    adjusted square ref name and redefined input handler with an arrow function

--- sudoku/src/components/Square.js
+++ sudoku/src/components/Square.js
@@ -3,31 +3,24 @@ import React from 'react';
 export default class Square extends React.Component {
   constructor(props) {
     super(props)
-    this.numInput = React.createRef()
+    this.inputRef = React.createRef()
     this.state = {
       row: props.row,
       position: props.position,
       value: props.value,
       isSelected: props.isSelected
     }
-
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.focusNumInput = this.focusNumInput.bind(this)
   }
 
-  focusNumInput() {
-    this.numInput.current.focus()
-  }
-
-  handleInputChange(e){
+  handleInputChange = (e) => {
     const newValue = e.target.value
     if (((newValue > 0 && newValue < 10) && newValue !== ' ') || newValue === '') {
       this.setState({value: e.target.value}) // set value of square
       if (this.props.position >= 8) {
-        if(this.props.row >= 8) return // stop if we're at the last square on the last row
-        this.numInput.current.parentNode.nextSibling.childNodes[0].focus() // focus first square in next row
+        if (this.props.row >= 8) return // stop if we're at the last square on the last row
+        this.inputRef.current.parentNode.nextSibling.childNodes[0].focus() // focus first square in next row
       } else {
-        this.numInput.current.nextSibling.focus() // focus next square
+        this.inputRef.current.nextSibling.focus() // focus next square
       }
     }
   }
@@ -36,7 +29,7 @@ export default class Square extends React.Component {
     return (
     <input 
       className="square"
-      ref={this.numInput}
+      ref={this.inputRef}
       value={this.state.value}
       onChange={this.handleInputChange}
       onFocus={() => this.setState({isSelected: true})}
